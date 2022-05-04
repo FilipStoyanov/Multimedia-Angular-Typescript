7721d7fb5b11ac12ee01c698960271d21778c29c
unknown <filipstoianov@abv.bg>
2022-05-04 19:40:12 +0300

    Fixes

--- MovieSystem/src/app/actions/dist/image.actions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.addImage = void 0;
-var store_1 = require("@ngrx/store");
-exports.addImage = store_1.createAction('addImage', store_1.props());
--- MovieSystem/src/app/actions/dist/user.actions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.addUser = void 0;
-var store_1 = require("@ngrx/store");
-exports.addUser = store_1.createAction('addUser', store_1.props());
